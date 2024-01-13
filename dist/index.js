e3cabbd7704b562cbdd1556de7e39587ec8f629c
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-01-12 17:28:42 -0800

    set up config file to remmeber

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let employee = {
+    id: 1,
+    name: 'Ray',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+employee.retire(new Date());
+//# sourceMappingURL=index.js.map
